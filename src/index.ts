a18e5748de222371e25fb3a82d400d3bff5a8406
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-01-20 01:54:25 +0500

    modify for schema and resolvers

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,32 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import dotenv from 'dotenv';
 
+import { connectDB } from './config/dbConnection';
+import { typeDefs } from './schemas/allSchemas';
+import { resolvers } from './resolvers/allResolvers'
 
-const typeDefs = `
-  type Query {
-    hello: String
-  }
-`;
-
-// Define the resolvers
-const resolvers = {
-  Query: {
-    hello: () => "Hello, Graphql!" 
-  }
-};
-
+dotenv.config();
+connectDB();
 
 const server: ApolloServer = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers,
+  formatError: (err) => {
+    // Customize the error response
+    return {
+      message: err.message, // Only include the message
+      success: false, // Optional: Add custom fields like success flag
+    };
+  },
 });
 
-const startServer = async() : Promise<void> => {
-    const { url } = await startStandaloneServer(server, {
-        listen: { port: 4000 }, 
-      });
-      
-      console.log(`🚀 Server ready at: ${url}`);
+const startServer = async (): Promise<void> => {
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+
+  console.log(`🚀 Server ready at: ${url}`);
 }
 
 startServer();

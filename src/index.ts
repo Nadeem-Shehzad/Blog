c9421d72550fdc5f44ee74be9c94b132950ad542
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-01-21 11:34:40 +0500

    added context for token validation

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,28 @@ import dotenv from 'dotenv';
 import { connectDB } from './config/dbConnection';
 import { typeDefs } from './schemas/allSchemas';
 import { resolvers } from './resolvers/allResolvers'
+import { IncomingMessage } from 'http';
+import { MyContext } from './utils/types';
+import { tokenValidation } from './middlewares/tokenValidation';
 
 dotenv.config();
 connectDB();
 
 const server: ApolloServer = new ApolloServer({
   typeDefs,
-  resolvers,
-  formatError: (err) => {
-    // Customize the error response
-    return {
-      message: err.message, // Only include the message
-      success: false, // Optional: Add custom fields like success flag
-    };
-  },
+  resolvers
 });
 
 const startServer = async (): Promise<void> => {
   const { url } = await startStandaloneServer(server, {
+    context: async ({ req }: { req: IncomingMessage }): Promise<MyContext> => {
+      const contextData = await tokenValidation({ req });
+      return {
+        userId: contextData.userId,
+        email: contextData.email,
+        role: contextData.role
+      }
+    },
     listen: { port: 4000 },
   });
 

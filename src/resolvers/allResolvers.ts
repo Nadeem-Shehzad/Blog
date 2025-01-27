9b9028465fcadf185b0ebcf19a705c37430c7ab7
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-01-27 17:34:11 +0500

    added blog resolver

--- src/resolvers/allResolvers.ts
+++ src/resolvers/allResolvers.ts
@@ -1,13 +1,16 @@
 
 import { userResolver } from './user'
+import {blogResolver} from './blog';
 
 export const resolvers = {
     Query: {
-        ...userResolver.Query
+        ...userResolver.Query,
+        ...blogResolver.Query
     },
 
     Mutation: {
-        ...userResolver.Mutation
+        ...userResolver.Mutation,
+        ...blogResolver.Mutation
     }
 }
 

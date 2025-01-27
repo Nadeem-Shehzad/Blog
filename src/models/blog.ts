f1155b2375ee73c249de1bf40ced601f9d8df4df
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-01-27 17:32:51 +0500

    added blog model

---
+++ src/models/blog.ts
@@ -0,0 +1,63 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { IBlog } from '../utils/types'
+
+export interface BlogDocument extends IBlog, Document { }
+
+const blogSchema: Schema = new Schema({
+    creater_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Users'
+    },
+    title: {
+        type: String,
+        min: 3,
+        max: 25,
+        trim: true,
+        required: [true, 'Add title'],
+    },
+    description: {
+        type: String,
+        max: 100,
+        default: ''
+    },
+    image: {
+        public_id: { // needs public_id to delete it from cloudinary
+            type: String,
+            required: [true,'image public_id missing!']
+        },
+        url: {
+            type: String,
+            required: [true,'image url missing!']       
+        }
+    },
+    likes: {
+        type: [{
+            userId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Users',
+                required: true,
+            },
+        }],
+        default: []
+    },
+    comments: {
+        type: [{
+            userId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Users',
+                require: true
+            },
+            comment:{
+                type: String,
+                max: 100,
+                require: true
+            }
+        }],
+        default: []
+    }
+}, { timestamps: true });
+
+const Blog = mongoose.model<BlogDocument>('Blog', blogSchema);
+
+export default Blog;

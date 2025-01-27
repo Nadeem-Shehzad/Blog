a583fdaa9982e346337e34e0e04bb9c2226682b4
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-01-27 17:36:17 +0500

    added blog schema

--- src/schemas/allSchemas.ts
+++ src/schemas/allSchemas.ts
@@ -1,8 +1,10 @@
 import { gql } from 'graphql-tag';
 
-import { userSchema } from './user'
+import { userSchema } from './user';
+import { blogSchema } from './blog';
 
 
 export const typeDefs = gql`
  ${userSchema}
+ ${blogSchema}
 `;

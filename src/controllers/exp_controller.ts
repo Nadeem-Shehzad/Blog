9726cae8061847e9e2ebf232b61655f6cfcf2089
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-01-27 17:32:10 +0500

    added delete file api

--- src/controllers/exp_controller.ts
+++ src/controllers/exp_controller.ts
@@ -1,25 +1,13 @@
 import { Request, Response } from "express";
-import User from '../models/user';
 import cloudinary from '../config/cloudinaryConfig';
 import fileUpload from "express-fileupload";
 
 
+export const upload = async (req: Request, res: Response) => {
 
-export const signUp = async (req: Request, res: Response) => {
-
-    const userData = req.body;
-    const user = await User.findUserByEmail(userData.email);
-
-    if (user) {
-        return res.status(400).json({ success: false, message: 'User Exist.', data: null });
-    }
-
-    const hashedPassword = await User.hashedPassword(userData.password);
-
-    let imageUploadResult = '';
     if (req.files && req.files.image) {
         const file = req.files.image as fileUpload.UploadedFile;
-        
+
         const result = await cloudinary.uploader.upload(file.tempFilePath, {
             public_id: `${Date.now()}`,
             resource_type: "auto",
@@ -28,17 +16,36 @@ export const signUp = async (req: Request, res: Response) => {
 
         // check image is uploaded successfully or not
         if (result && result.secure_url) {
-            imageUploadResult = result.secure_url;
+            const imageData = {
+                public_id: result.public_id,
+                url: result.secure_url
+            }
+            return res.status(201).json({ success: true, message: 'image uploaded.', data: imageData });
         }
+        return res.status(500).json({ success: false, message: 'image not uploaded.', data: '' });
     }
+};
 
-    const newUser = await User.create({
-        ...userData,
-        password: hashedPassword,
-        image: imageUploadResult
-    });
 
-    newUser
-        ? res.status(201).json({ success: true, message: 'User SignedUp.', data: newUser })
-        : res.status(500).json({ success: false, message: 'User not SignedUp.', data: null });
+export const deleteFile = async (req: Request, res: Response) => {
+
+    const imageId = req.body.imageId;
+
+    try {
+        const result = await cloudinary.uploader.destroy(
+            imageId,
+            { resource_type: "image", invalidate: true }
+        );
+
+        if (result.result === "ok") {
+            return res.status(500).json({ success: true, message: 'file deleted.', data: '' });
+        } else if (result.result === "not found") {
+            return res.status(500).json({ success: false, message: 'file not found on Cloudinary.', data: '' });
+        } else {
+            return res.status(500).json({ success: false, message: 'Failed to delete the file.', error: result.result });
+        }
+
+    } catch (error) {
+        return res.status(500).json({ success: false, message: 'Error occurred while deleting the file:', error: error });
+    }
 };

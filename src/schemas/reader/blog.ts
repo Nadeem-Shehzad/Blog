8abdeca74abe77e442b64f4ca434f6d10b7ffada
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-01-29 08:16:22 +0500

    added like and bookmark

--- src/schemas/reader/blog.ts
+++ src/schemas/reader/blog.ts
@@ -1,14 +1,36 @@
-import {gql} from 'graphql-tag';
+import { gql } from 'graphql-tag';
 
 export const readerBlogSchema = gql`
 
- type LikedBlogResponse{
-    success: Boolean!
-    message: String!
+ # query
+ type BookMark{
+  userId: String!
+  blogId: String!
+ }
+
+ type BookMarkQueryResponse{
+   success: Boolean!
+   message: String!
+   data: [BookMark!]
  } 
 
+ type Query{
+  getMyBookMarks: BookMarkQueryResponse!
+  getMyLikedBlogs: BookMarkQueryResponse!
+ }
+
+ # mutation
+ type BookMarkMutationResponse{
+   success: Boolean!
+   message: String!
+ }
+
  type Mutation{
-    likeBlog(blogId: String!): LikedBlogResponse!
+   likeBlog(blogId: String!): BlogMutationResponse!
+   bookmarkBlog(blogId: String!): BlogMutationResponse!
+   deleteBookMark(blogId: String!):BlogMutationResponse!
+   # follow/unfollow author
+   # comment on blog
  }
 
 `;

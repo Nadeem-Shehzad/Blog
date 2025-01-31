655a86f36cbe039977c7b0e9c3f6cbb382f573bf
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-02-01 02:35:55 +0500

    added authors and followuser type

--- src/schemas/dataTypes/userTypes/queryTypes.ts
+++ src/schemas/dataTypes/userTypes/queryTypes.ts
@@ -18,3 +18,22 @@ export const QueryResponse = `
     message: String!
     data: [User]
 }`;
+
+
+export const FollowUserType = `
+ type FollowUser{
+    id: ID!
+    username:String!
+    email:String!
+    role: String!
+    following: [User]  
+    followers: [User]  
+}`;
+
+
+export const FollowUserQueryResponse = `
+ type FollowUserQueryResponse{
+    success: Boolean!
+    message: String!
+    data: [FollowUser]
+}`;

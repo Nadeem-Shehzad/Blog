e123a9a1381758164be8f4b1201c575b2a77d4a5
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-02-01 02:37:24 +0500

    modify names

--- src/schemas/allSchemas.ts
+++ src/schemas/allSchemas.ts
@@ -4,7 +4,7 @@ import { authSchema } from './common/auth';
 import { authorBlogSchema } from './author/author';
 import { commonBlogSchema } from './common/blog';
 import { adminBlogSchema } from './admin/admin';
-import {readerSchema} from './reader/reader';
+import { readerSchema } from './reader/reader';
 
 
 export const typeDefs = gql`

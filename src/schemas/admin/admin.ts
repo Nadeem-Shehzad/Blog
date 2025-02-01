090ec3ba19f59a33b504269c5d7055bf0747bb53
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-02-01 17:18:34 +0500

    added delete comment and user func.

--- src/schemas/admin/admin.ts
+++ src/schemas/admin/admin.ts
@@ -1,15 +1,26 @@
 import { gql } from 'graphql-tag';
 
-import { UserType, QueryResponse } from '../dataTypes/userTypes/queryTypes';
+import { UserType, QueryResponse, SingleUserQueryResponse } from '../dataTypes/userTypes/queryTypes';
+import {BlogMutationResponse} from '../dataTypes/blogTypes/mutationTypes'
 
-export const adminBlogSchema = gql`
+export const adminSchema = gql`
 
 # types
 ${UserType}
 ${QueryResponse}
+${SingleUserQueryResponse}
+
+${BlogMutationResponse}
 
 type Query{
     getReaders: QueryResponse
 }
 
+type Mutation{
+    blockUser(userId:String!): SingleUserQueryResponse
+    unBlockUser(userId:String!): SingleUserQueryResponse
+    deleteCommentByadmin(blogId:String!, commentId:String!): BlogMutationResponse
+    deleteBlogByAdmin(blogId:String!): BlogMutationResponse
+    deleteUserAccount(userId: String!): SingleUserQueryResponse
+}
 `;

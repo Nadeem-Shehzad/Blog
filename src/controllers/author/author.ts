206e492cd1019fb8910f364358488c1b2979dc40
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-02-01 17:08:32 +0500

    modify code

--- src/controllers/author/author.ts
+++ src/controllers/author/author.ts
@@ -1,100 +1,98 @@
-import { IBlog, BlogResponse, MyContext,FollowUserResponse } from '../../utils/types';
+import { IBlog, BlogResponse, MyContext, FollowUserResponse } from '../../utils/types';
 import Blog from '../../models/blog';
 import User from '../../models/user';
-import { compose, authMiddleware, checkRole, ErrorHandling } from '../../middlewares/common';
-
-
-// <------- Author Query ------->
-
-export const qGetDraftedBlogs = compose(ErrorHandling, authMiddleware, checkRole(['Author']))(async (_: any, __: any, context: MyContext): Promise<BlogResponse> => {
-
-    const draftedBlogs = await Blog.find({ $and: [{ status: 'draft' }, { creater_id: context.userId }] });
-    return { success: true, message: 'All Drafted Blogs', data: draftedBlogs };
-});
-
-
-export const qGetMyBlogs = compose(ErrorHandling, authMiddleware, checkRole(['Author']))(async (_: any, __: any, context: MyContext): Promise<BlogResponse> => {
-
-    const myBlogs = await Blog.find({ $and: [{ status: 'published' }, { creater_id: context.userId }] });
-    return { success: true, message: 'All Your Blogs', data: myBlogs };
-});
-
-
-export const qGetMyFollowers = compose(ErrorHandling, authMiddleware, checkRole(['Author']))(async (_: any, __: any, context: MyContext): Promise<FollowUserResponse> => {
-
-    const myFollowers = await User.findById(context.userId).populate('followers', 'username email ').lean();
-    return { success: true, message: 'All Followers', data: myFollowers?.followers || []};
-});
-
-// <------- End Author Query ------->
+import { compose, authMiddleware, checkRole, ErrorHandling, IsBlogExists } from '../../middlewares/common';
 
 
 
+export const qGetDraftedBlogs = compose(ErrorHandling, authMiddleware, checkRole(['Author']))
+    (async (_: any, __: any, context: MyContext): Promise<BlogResponse> => {
 
-// <------- Author Mutation ------->
-
-export const mCreateBlog = compose(ErrorHandling, authMiddleware, checkRole(['Author']))(async (_: any, { blogData }: { blogData: IBlog }, context: MyContext): Promise<BlogResponse> => {
-
-    const { title, description, image, tags, status } = blogData;
-
-    const finalStatus = status || "draft";
-
-    const newBlog = await Blog.create({
-        creater_id: context.userId,
-        title: title,
-        description: description,
-        image: {
-            public_id: image.public_id,
-            url: image.url
-        },
-        tags: tags || [],
-        status: finalStatus
+        const draftedBlogs = await Blog.find({ $and: [{ status: 'draft' }, { creater_id: context.userId }] });
+        return { success: true, message: 'All Drafted Blogs', data: draftedBlogs };
     });
 
-    return { success: true, message: `Blog ${finalStatus === "published" ? "published" : "saved as draft"}`, data: newBlog };
 
-});
+export const qGetMyBlogs = compose(ErrorHandling, authMiddleware, checkRole(['Author']))
+    (async (_: any, __: any, context: MyContext): Promise<BlogResponse> => {
 
-
-export const mUpdateBlog = compose(ErrorHandling, authMiddleware, checkRole(['Author']))(async (_: any, { blogId, blogData }: { blogId: string, blogData: IBlog }, context: MyContext): Promise<BlogResponse> => {
-
-    const blog = await Blog.findById(blogId);
-    if (!blog) {
-        return { success: false, message: 'Blog not Exist!', data: null };
-    }
-
-    if (blog.creater_id.toString() !== context.userId) {
-        return { success: false, message: 'Access Denied!', data: null };
-    }
-
-    const updatedBlog = await Blog.findByIdAndUpdate(
-        blogId,
-        {
-            $set: {
-                ...blogData
-            }
-        },
-        { new: true }
-    );
-
-    return { success: true, message: 'Blog Updated.', data: updatedBlog };
-});
+        const myBlogs = await Blog.find({ $and: [{ status: 'published' }, { creater_id: context.userId }] });
+        return { success: true, message: 'All Your Blogs', data: myBlogs };
+    });
 
 
-export const mDeleteBlog = compose(ErrorHandling, authMiddleware, checkRole(['Author']))(async (_: any, { blogId }: { blogId: string }, context: MyContext): Promise<BlogResponse> => {
+export const qGetMyFollowers = compose(ErrorHandling, authMiddleware, checkRole(['Author']))
+    (async (_: any, __: any, context: MyContext): Promise<FollowUserResponse> => {
 
-    const blog = await Blog.findById(blogId);
-    if (!blog) {
-        return { success: false, message: 'Blog not Exist!', data: null };
-    }
+        const myFollowers = await User.findById(context.userId).populate('followers', 'username email ').lean();
+        return { success: true, message: 'All Followers', data: myFollowers?.followers || [] };
+    });
 
-    if (blog.creater_id.toString() !== context.userId) {
-        return { success: false, message: 'Access Denied!', data: null };
-    }
 
-    const deletedBlog = await Blog.findByIdAndDelete(blogId);
 
-    return { success: true, message: 'Blog Deleted.', data: deletedBlog };
-});
+export const mCreateBlog = compose(ErrorHandling, authMiddleware, checkRole(['Author']))
+    (async (_: any, { blogData }: { blogData: IBlog }, context: MyContext): Promise<BlogResponse> => {
 
-// <------- End Author Mutation ------->
+        const { title, description, image, tags, status } = blogData;
+
+        const finalStatus = status || "draft";
+
+        const newBlog = await Blog.create({
+            creater_id: context.userId,
+            title: title,
+            description: description,
+            image: {
+                public_id: image.public_id,
+                url: image.url
+            },
+            tags: tags || [],
+            status: finalStatus
+        });
+
+        return { success: true, message: `Blog ${finalStatus === "published" ? "published" : "saved as draft"}`, data: newBlog };
+
+    });
+
+
+export const mUpdateBlog = compose(ErrorHandling, authMiddleware, checkRole(['Author']))
+    (async (_: any, { blogId, blogData }: { blogId: string, blogData: IBlog }, context: MyContext): Promise<BlogResponse> => {
+
+        const blog = await Blog.findById(blogId);
+        if (!blog) {
+            return { success: false, message: 'Blog not Exist!', data: null };
+        }
+
+        if (blog.creater_id.toString() !== context.userId) {
+            return { success: false, message: 'Access Denied!', data: null };
+        }
+
+        const updatedBlog = await Blog.findByIdAndUpdate(
+            blogId,
+            {
+                $set: {
+                    ...blogData
+                }
+            },
+            { new: true }
+        );
+
+        return { success: true, message: 'Blog Updated.', data: updatedBlog };
+    });
+
+
+export const mDeleteBlog = compose(ErrorHandling, authMiddleware, checkRole(['Author']))
+    (async (_: any, { blogId }: { blogId: string }, context: MyContext): Promise<BlogResponse> => {
+
+        const blog = await Blog.findById(blogId);
+        if (!blog) {
+            return { success: false, message: 'Blog not Exist!', data: null };
+        }
+
+        if (blog.creater_id.toString() !== context.userId) {
+            return { success: false, message: 'Access Denied!', data: null };
+        }
+
+        const deletedBlog = await Blog.findByIdAndDelete(blogId);
+
+        return { success: true, message: 'Blog Deleted.', data: deletedBlog };
+    });
